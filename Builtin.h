686b2eb8edcbee6dc9382e0bc5c1eda5d31c55b1
Ritika Laddha <69119436+Ritika54@users.noreply.github.com>
2022-06-05 10:14:28 +0530

    update

--- Builtin.h
+++ Builtin.h
@@ -728,11 +728,40 @@ void mergeSort(int arr[],int low,int high){
         merge(arr,low,mid,high);
     }
 }
+void insertion(int arr[], int left, int right)
+{
+    for (int i = left + 1; i <= right; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > temp)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
 //TIM SORT
 //BEST case = O(n)
 //Worst/Avg = O(nlogn)
+const int RUN = 32;
 void timSort(int arr,int n){
-
+    for (int i = 0; i < n; i+=RUN){
+        insertion(arr, i, min((i+RUN-1),(n-1)));
+    }
+ 
+    for (int size = RUN; size < n;size = 2*size)
+    {
+        for (int left = 0; left < n;left += 2*size)
+        {
+            int mid = left + size - 1;
+            int right = min((left + 2*size - 1),(n-1));
+ 
+              if(mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
 }
 
 //TECH 13
